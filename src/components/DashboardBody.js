b8fc4a41ddbd34e44bc4fcb444428498c87e8df9
souhirbeji <bejisouir2@gmail.com>
2023-05-24 18:13:54 +0100

    sans darkmode

--- src/components/DashboardBody.js
+++ src/components/DashboardBody.js
@@ -1,18 +1,9 @@
-import { useState } from "react";
 import { Card, Text, Metric, AreaChart, Title } from "@tremor/react";
 import { dataFormatter, chartdata } from "./../data/Areachart";
-import { FiSun, FiMoon } from "react-icons/fi";
 
 function DashboardBody() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
   return (
-    <div className={`h-full basis-5/6 ${darkMode ? "bg-blue-950 text-white" : "bg-blue-100 text-black"}`}>
-  
+    <div className="h-full basis-5/6 bg-blue-100 text-center">
       <div className="overflow-y-auto h-full basis-5/6">
         <div className="flex flex-col">
           <div className="grid grid-rows-1 grid-cols-8 gap-2 mt-14 overflow-y-auto">
@@ -21,11 +12,11 @@ function DashboardBody() {
               <Metric>20°C</Metric>
             </Card>
             <Card className="col-start-5 col-span-2 h-40">
-              <Text>humidité</Text>
+              <Text>humidité </Text>
               <Metric>30%</Metric>
             </Card>
           </div>
-          <div className="grid grid-rows-1 grid-cols-8 gap-2 mt-6">
+          <div className="grid grid-rows-1 grid-cols-8 gap-2 mt-6 ">
             <Card className="col-start-3 col-end-7">
               <Title>Newsletter revenue over time (USD)</Title>
               <div className="h-72 mt-4">
@@ -41,16 +32,6 @@ function DashboardBody() {
           </div>
         </div>
       </div>
-      <div className="absolute top-4 right-4">
-        <button
-          className={`bg-transparent p-2 rounded-lg ${darkMode ? "text-gray-200" : "text-gray-700"}`}
-          onClick={toggleDarkMode}
-        >
-          {darkMode ? <FiMoon size={24} /> : <FiSun size={24} />}
-        </button>
-      </div>
     </div>
   );
-}
-
-export default DashboardBody;
+}export default DashboardBody;
