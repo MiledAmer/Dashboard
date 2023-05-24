484f416d8f95d976e36db692de5e29b60cfe586c
souhirbeji <bejisouir2@gmail.com>
2023-05-24 17:39:50 +0100

    salem hak el logoo

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,13 +1,15 @@
+import logo from "../Assets/logo.png";
 export default function Example() {
   return (
     <>
-      <div className="flex -space-x-2 overflow-hidden">
-        <img
-          className="inline-block h-30 w-30 rounded-full ring-2 ring-white bg-contain"
-          src="https://e7.pngegg.com/pngimages/620/4/png-clipart-tunisie-telecom-wynsys-sfax-telecommunication-telecom-tower-text-logo.png"
-          alt=""
+      <div className="flex justify-center  overflow-hidden ">
+        <img 
+          className="rounded-full w-20 h-20 "
+          src={logo} 
+          alt="tunisietelecom"
         />
       </div>
+      
     </>
-  )
+  );
 }
